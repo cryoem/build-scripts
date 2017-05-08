2cfce379b67648e751be1a3a95d7563bdf682427
shadow_walker <shadowwalkersb@gmail.com>
2017-05-08 18:45:05 -0500

    set miniconda file conditionally

--- docker-images/scripts/conda_environment.sh
+++ docker-images/scripts/conda_environment.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-MINICONDA_FILE="Miniconda2-latest-Linux-x86_64.sh"
+case $1 in
+    'centos6') os_label="Linux"   ;;
+    'centos7') os_label="Linux"   ;;
+    'mac')     os_label="MacOSX"  ;;
+    'win')     os_label="Windows" ;;
+esac
+
+MINICONDA_FILE="Miniconda2-latest-${os_label}-x86_64.sh"
 
 curl -v -L -O https://repo.continuum.io/miniconda/$MINICONDA_FILE
 bash $MINICONDA_FILE -b
