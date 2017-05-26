523dd1e27fa54f64d8437d44c379a996b152ec1b
shadow_walker <shadowwalkersb@gmail.com>
2017-05-26 18:30:34 -0500

    call docker script if cronjob is for centos6

--- cronjob.sh
+++ cronjob.sh
@@ -28,6 +28,7 @@ MYDIR=$(cd $(dirname $0) && pwd -P)
 EMAN_REPO_DIR="${HOME}"/workspace/eman2-src
 EMAN_REICPE_DIR="${EMAN_REPO_DIR}"/recipes/eman
 INSTALLERS_DIR="${HOME}"/workspace/eman-installers
+OUTPUT_DIR_CENTOS6="${HOME}"/workspace/centos6
 CONSTRUCT_YAML_DIR="${HOME}"/workspace/build-scripts/constructor
 
 CONSTRUCTOR_OUTPUT_FILENAME="eman${version}.${os_label}.${ctor_out_ext}"
@@ -41,15 +42,23 @@ cd "${EMAN_REPO_DIR}"
 git checkout ${branch}
 git pull --rebase
 
-# Conda-build eman
-source activate root
+if [ "$1" == "centos6" ];then
+    bash "${MYDIR}/run_docker_build.sh" cryoem/centos6 \
+                                        "${EMAN_REPO_DIR}" \
+                                        "${OUTPUT_DIR_CENTOS6}"
 
-bash "${MYDIR}/build_and_package.sh" "${EMAN_REICPE_DIR}" \
-                                     "${INSTALLERS_DIR}" \
-                                     "${CONSTRUCT_YAML_DIR}"
+    cp -av "${OUTPUT_DIR_CENTOS6}/installers/${CONSTRUCTOR_OUTPUT_FILENAME}" "${OUTPUT_DIR_CENTOS6}/installers/${UPLOAD_FILENAME}"
+else
+    # Conda-build eman
+    source activate root
 
-# Upload installer
-cp -av "${INSTALLERS_DIR}/${CONSTRUCTOR_OUTPUT_FILENAME}" "${INSTALLERS_DIR}/${UPLOAD_FILENAME}"
+    bash "${MYDIR}/build_and_package.sh" "${EMAN_REICPE_DIR}" \
+                                         "${INSTALLERS_DIR}" \
+                                         "${CONSTRUCT_YAML_DIR}"
+
+    # Upload installer
+    cp -av "${INSTALLERS_DIR}/${CONSTRUCTOR_OUTPUT_FILENAME}" "${INSTALLERS_DIR}/${UPLOAD_FILENAME}"
+fi
 
 if [ "$1" != "win" ];then
     cmd="rsync -avzh --stats"
