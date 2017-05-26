9723352d74742ee1a52fa60e93bc6aa7c2f8b2eb
shadow_walker <shadowwalkersb@gmail.com>
2017-05-25 20:35:07 -0500

    rename eman2 repo dir: eman2-src -> eman2-cron

--- cronjob.sh
+++ cronjob.sh
@@ -34,7 +34,7 @@ fi
 set -xe
 
 MYDIR=$(cd $(dirname $0) && pwd -P)
-EMAN_REPO_DIR="${HOME}"/workspace/eman2-src
+EMAN_REPO_DIR="${HOME}"/workspace/eman2-cron
 EMAN_REICPE_DIR="${EMAN_REPO_DIR}"/recipes/eman
 INSTALLERS_DIR="${HOME}/workspace/${1}-installers"
 CONSTRUCT_YAML_DIR="${HOME}"/workspace/build-scripts/constructor
