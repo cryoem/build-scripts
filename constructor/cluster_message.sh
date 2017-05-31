b961125a2a1cf947a9d471851b84195701d5f410
shadow_walker <shadowwalkersb@gmail.com>
2017-05-31 16:29:18 -0500

    remove detailed instructions and refer to wiki

--- constructor/cluster_message.sh
+++ constructor/cluster_message.sh
@@ -4,11 +4,9 @@ cat <<EOF
 
 INSTALLATION IS NOW COMPLETE
 
-Important note for Linux Cluster use:
-If you are using EMAN2/SPARX/SPHIRE on a cluster, the version of OpenMPI we provide may not work with your batch queueing system, meaning you would not be able to run jobs on more than one node at a time. If this is true:
-- run 'utils/uninstall_openmpi.sh' to remove the OpenMPI we provided
-- make sure that the correct OpenMPI for your cluster is in your path. You should be able to run 'mpicc' and get a message like 'gcc: no input files'
- (note that it is critical that OpenMPI be compiled with '--disable-dlopen', which may or may not be true on your cluster. You may need to consult a sysadmin.)
-- run 'utils/install_pydusa.sh' to rebuild Pydusa using the system installed OpenMPI
+Please, go to http://blake.bcm.edu/emanwiki/EMAN2/Install/BinaryInstallAnaconda
+for detailed installation instructions, testing and troubleshooting information.
+If this installation is on a Linux cluster,
+you will require additional steps before installation is complete!
 
 EOF
