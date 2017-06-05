c1e87cf8c44353812b591e44cbe005c57d8794a2
shadow_walker <shadowwalkersb@gmail.com>
2017-06-05 01:19:18 -0500

    add function print_usage and call it

--- cronjob.sh
+++ cronjob.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-if [ $# -lt 1 ] || [ $# -gt 2 ];then
+function print_usage(){
     echo
     echo -e '\033[35m'"  Usage: $(basename ${0})    [os-label (mac, centos6, centos7, win)]    [branch (optional)]"'\033[0m'
     echo
     exit 1
+}
+
+if [ $# -lt 1 ] || [ $# -gt 2 ];then
+    print_usage
 fi
 
 version="2.2"
@@ -14,6 +18,7 @@ case $1 in
     'centos7') os_label="linux64"; distro_label=".centos7"; ctor_out_ext="sh";  upload_ext="daily.exe";  ;;
     'mac')     os_label="mac";     distro_label="";         ctor_out_ext="sh";  upload_ext="daily1.sh";  ;;
     'win')     os_label="win64";   distro_label="";         ctor_out_ext="exe"; upload_ext="daily1.exe"; ;;
+    *)         print_usage; ;;
 esac
 
 if [ $# -eq 2 ];then
