5524238928a64f764de3e8e2d0e802c79508d410
shadow_walker <shadowwalkersb@gmail.com>
2017-06-05 01:44:30 -0500

    update usage string and exit code according to conventions

--- cronjob.sh
+++ cronjob.sh
@@ -2,9 +2,9 @@
 
 function print_usage(){
     echo
-    echo -e '\033[35m'"  Usage: $(basename ${0})    [os-label (mac, centos6, centos7, win)]    [branch (optional)]"'\033[0m'
+    echo -e '\033[35m'"  Usage: $(basename ${0})    {mac|centos6|centos7|win}    [branch-to-checkout]"'\033[0m'
     echo
-    exit 1
+    exit 64
 }
 
 if [ $# -lt 1 ] || [ $# -gt 2 ];then
