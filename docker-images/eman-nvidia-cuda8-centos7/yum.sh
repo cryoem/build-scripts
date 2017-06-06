71c9b05769b9a2972de82d7bd0d3588f99a67307
shadow_walker <shadowwalkersb@gmail.com>
2017-06-06 00:54:31 -0500

    initial

---
+++ docker-images/eman-nvidia-cuda8-centos7/yum.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+yum install -y \
+               gcc \
+               gcc-c++ \
+               mesa-libGLU-devel \
+               libXext-devel \
+               libXrender-devel \
+               libSM-devel \
+               libX11-devel \
+               fontconfig && \
+yum clean all
