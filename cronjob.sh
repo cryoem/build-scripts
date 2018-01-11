b49865f95e6993c89e4ddb476c4f6baa78a382a1
shadow_walker <shadowwalkersb@gmail.com>
2018-01-11 00:09:23 -0600

    release versions: 2.21

--- cronjob.sh
+++ cronjob.sh
@@ -6,15 +6,15 @@ function print_usage(){
 }
 
 
-version="2.2"
+version="2"
 
 case $# in
     1|2)
         case $1 in
-            'centos6') os_label="linux64"; distro_label=".${1}"; ctor_out_ext="sh";  upload_ext="daily1.sh";  ;;
-            'centos7') os_label="linux64"; distro_label=".${1}"; ctor_out_ext="sh";  upload_ext="daily.exe";  ;;
-            'mac')     os_label="mac";     distro_label="";      ctor_out_ext="sh";  upload_ext="daily1.sh";  ;;
-            'win')     os_label="win64";   distro_label="";      ctor_out_ext="exe"; upload_ext="daily1.exe"; ;;
+            'centos6') os_label="linux64"; distro_label=".${1}"; ctor_out_ext="sh";  upload_ext="unstable.sh";  ;;
+            'centos7') os_label="linux64"; distro_label=".${1}"; ctor_out_ext="sh";  upload_ext="unstable.sh";  ;;
+            'mac')     os_label="mac";     distro_label="";      ctor_out_ext="sh";  upload_ext="unstable.sh";  ;;
+            'win')     os_label="win64";   distro_label="";      ctor_out_ext="exe"; upload_ext="unstable.exe"; ;;
             *)         print_usage; ;;
         esac
 
