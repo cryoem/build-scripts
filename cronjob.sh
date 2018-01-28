685998899f5dd2a37f9827a123d9f638e8aacd24
shadow_walker <shadowwalkersb@gmail.com>
2018-01-27 23:43:43 -0600

    build-scripts-cron

--- cronjob.sh
+++ cronjob.sh
@@ -30,7 +30,7 @@ MYDIR=$(cd $(dirname $0) && pwd -P)
 EMAN_REPO_DIR="${HOME}"/workspace/eman2-cron
 EMAN_REICPE_DIR="${EMAN_REPO_DIR}"/recipes/eman
 INSTALLERS_DIR="${HOME}/workspace/${1}-installers"
-CONSTRUCT_YAML_DIR="${HOME}"/workspace/build-scripts/constructor
+CONSTRUCT_YAML_DIR="${HOME}"/workspace/build-scripts-cron/constructor
 
 CONSTRUCTOR_OUTPUT_FILENAME="eman2.${os_label}.${ctor_out_ext}"
 
