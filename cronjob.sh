cfe00d8c7bfd980c22004e5c1d8da3985fdb9064
shadow_walker <shadowwalkersb@gmail.com>
2018-01-29 00:38:19 -0600

    don't cleanup, cleanup in test script [ci skip]

--- cronjob.sh
+++ cronjob.sh
@@ -57,7 +57,6 @@ else
                                          "${INSTALLERS_DIR}" \
                                          "${CONSTRUCT_YAML_DIR}"
 
-    rm -rf eman2-linux64/ eman2-mac/
     bash "${EMAN_REPO_DIR}"/tests/test_binary_installation.sh "${INSTALLERS_DIR}"/"${CONSTRUCTOR_OUTPUT_FILENAME}"
 fi
 
